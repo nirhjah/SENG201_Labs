1fb913fbbbfb40e3b0947dbe67812da1ee95421b
nirhjah <34588618+nirhjah@users.noreply.github.com>
2021-05-27 21:26:59 +1200

    Lab 4

---
+++ SENG201/src/lab4/RemoteControllable.java
@@ -0,0 +1,8 @@
+package lab4;
+
+public interface RemoteControllable {
+	
+	public String getStatusReport();
+	public void updateMission(String newMission);
+
+}
---
+++ SENG201/src/lab4/Rover.java
@@ -0,0 +1,34 @@
+package lab4;
+
+public class Rover {
+	
+	private double longitude;
+	private double latitude;
+
+	
+	
+	public void setLocation(double latitude, double longitude) {
+		this.latitude = latitude;
+		this.longitude = longitude;
+		
+	}
+	
+	
+	public String getLocation() {
+		
+		return "The rover is located " + latitude +  ", " + longitude +  " on the planet.";
+		
+	}
+	
+	
+	
+	public String getStatusReport() {
+		return "The rover is driving to:" + "\n" + getLocation(); 
+	}
+	
+	
+	public void updateMission(String mission) {
+		
+	}
+	
+}
